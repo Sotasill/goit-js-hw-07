d1abbcb3f81c0d43216a72c9a311eb86babd92f8
Sotasill <164074705+Sotasill@users.noreply.github.com>
2024-07-17 01:12:17 +0300

    1

--- js/task-1.js
+++ js/task-1.js
@@ -1,38 +1,11 @@
-// Використай цей стартовий код і виконай рефакторинг. 
-// Після оголошення об'єкта ми додали виклики методів. 
-// У консоль будуть виведені результати їх роботи. Будь ласка, нічого там не змінюй.
+const categoriesList = document.querySelector('#categories');
+const items = categoriesList.querySelectorAll('.item');
 
-const customer = {
-  username: "Mango",
-  balance: 24000,
-  discount: 0.1,
-  orders: ["Burger", "Pizza", "Salad"],
+console.log(`Number of categories: ${items.length}`);
 
-  // Change code below this line
-
-  getBalance() {
-    return this.balance;
-  },
-  getDiscount() {
-    return this.discount;
-  },
-  setDiscount(value) {
-    this.discount = value;
-  },
-  getOrders() {
-    return this.orders;
-  },
-  addOrder(cost, order) {
-    this.balance -= cost - cost * this.discount;
-    this.orders.push(order);
-  },
-  // Change code above this line
-};
-
-customer.setDiscount(0.15);
-console.log(customer.getDiscount()); // 0.15
-customer.addOrder(5000, "Steak");
-console.log(customer.getBalance()); // 19750
-console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]
-
-
+items.forEach(item => {
+  const title = item.querySelector('h2').textContent;
+  const elementsCount = item.querySelectorAll('ul li').length;
+  console.log(`Category: ${title}`);
+  console.log(`Elements: ${elementsCount}`);
+});
